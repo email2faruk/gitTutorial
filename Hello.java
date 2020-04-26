983a2a57984d0692df3dce1a15a49eea0995a1a5
email2faruk <email2faruk@gmail.com>
2020-04-26 15:14:34 +0600

    Change greeting method

--- Hello.java
+++ Hello.java
@@ -2,11 +2,11 @@ public class Hello{
     public static void main(String[] args){
         System.out.println("VS Code is Awesome..");
 
-        greeting("Omar Faruk");
+        greeting("Git");
     }
 
     public static void greeting(String name)
     {
-        System.out.println("Welcome " + name);
+        System.out.println("By By  " + name);
     }
 }
